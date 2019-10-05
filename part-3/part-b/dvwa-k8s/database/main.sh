297d9a9e7c2f5c07100472c79ff8c130106f48a8
Brian Cheung <bcheung.dev@gmail.com>
2019-10-05 13:33:36 -0500

    lean up and add my.cnf to image

--- part-3/part-b/dvwa-k8s/database/main.sh
+++ part-3/part-b/dvwa-k8s/database/main.sh
@@ -5,9 +5,6 @@ chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 echo '[+] Starting mysql...'
 service mysql start && /bin/bash
 
-# echo '[+] Starting apache'
-# service apache2 start
-
 while true
 do
     tail -f /var/log/mysql/*.log
